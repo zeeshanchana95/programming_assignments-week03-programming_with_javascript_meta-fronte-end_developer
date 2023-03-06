6de9e3f4b1d8b161eb506bc05ea8884db9c08911
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-03-06 17:44:29 +0500

    Object Oriented Programming

---
+++ Assignment 02_OOP Programming/ooprogramming.js
@@ -0,0 +1,108 @@
+// // Task 1: Code a Person class
+// class Person{
+//     constructor(name = "Tom", age = 20, energy = 100) {
+//         this.name = name;
+//         this.age = age;
+//         this.energy = energy;
+//     }
+//     sleep() {
+//         this.energy += 10;
+//     }
+//     doSomethingFun() {
+//         this.energy -= 10;
+//     }
+// }
+
+// // Task 2: Code a Worker class
+// class Worker extends Person{
+//     constructor(name, age, energy, xp = 0, hourlyWage = 10) {
+//         super(name, age, energy);
+//         this.xp = xp;
+//         this.hourlyWage = hourlyWage;
+//     }
+//     goToWork() {
+//         this.xp += 10;
+//      }
+// }
+
+// // Task 3: Code an intern object, run methods
+// function intern() {
+//     var intern = new Worker();
+//     intern.name = "Bob";
+//     intern.age = 21;
+//     intern.energy = 110;
+//     intern.xp = 0;
+//     intern.hourlyWage = 10;
+//     intern.goToWork();
+//     return intern;
+// }
+
+// // Task 4: Code a manager object, methods
+// function manager() {
+//     var manager = Worker();
+//     manager.name = "Alice";
+//     manager.age = 30;
+//     manager.energy = 120;
+//     manager.xp = 100;
+//     manager.hourlyWage = 30;
+//     manager.doSomethingFun();
+//     return manager;
+// }
+
+// console.log(intern());
+// console.log(manager());
+
+
+// Task 1: Code a Person class
+class Person{
+    constructor(name="Tom", age=20, energy = 100) {
+        this.name = name;
+        this.age =age;
+        this.energy =energy;
+    }
+    sleep() {
+        this.energy += 10;
+    }
+    doSomethingFun() {
+        this.energy -= 10;
+    }
+}
+
+// Task 2: Code a Worker class
+class Worker extends Person{
+    constructor(name, age, energy, xp=0, hourlyWage=10) {
+        super(name,age,energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+    goToWork() {
+        this.xp += 10;
+    }
+}
+
+// Task 3: Code an intern object, run methods
+function intern() {
+    var intern = new Worker();
+    intern.name = "Bob";
+    intern.age = 21;
+    intern.energy = 110;
+    intern.xp = 0;
+    intern.hourlyWage = 10;
+    intern.goToWork();
+    return intern;
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+    var manager = new Worker();
+    manager.name = "Alice";
+    manager.age = 30;
+    manager.energy = 120;
+    manager.xp = 100;
+    manager.hourlyWage = 30;
+    manager.doSomethingFun();
+    return manager;
+}
+
+console.log(intern());
+console.log(manager());
